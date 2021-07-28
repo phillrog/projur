fedd3b608670d5fe966169c23f629c951fa0fa56
phillrog <phillrog@hotmail.com>
2021-07-28 20:48:36 -0300

    Entitdade de dominio

---
+++ src/ProJur.Cadastros.Domain/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using ProJur.Core.DomainObjects;
+using System;
+
+namespace ProJur.Cadastros.Domain
+{
+    public class Usuario : Entity
+    {
+        public string Nome { get; private set; }
+        public string SobreNome { get; private set; }
+        public string Email { get; private set; }
+        public DateTime DataNascimento { get; private set; }
+        public Escolaridade Escolaridade { get; set; }
+
+    }
+
+    public enum Escolaridade
+    {
+        Infantil = 0, 
+        Fundamental = 1, 
+        Medio = 2,
+        Superior = 3
+    }
+}
---
+++ src/ProJur.Core/DomainObjects/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProJur.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
+        }
+    }
+}
