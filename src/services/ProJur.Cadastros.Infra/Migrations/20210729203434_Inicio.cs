ddc93393db4e2aeacbd9a5b8d370637c5aabcc64
phillrog <phillrog@hotmail.com>
2021-07-29 17:35:08 -0300

    configurando migrations

---
+++ src/services/ProJur.Cadastros.Infra/Migrations/20210729203434_Inicio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProJur.Cadastros.Infra.Migrations
+{
+    public partial class Inicio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nome = table.Column<string>(type: "varchar(250)", nullable: false),
+                    Sobrenome = table.Column<string>(type: "varchar(250)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(250)", nullable: false),
+                    DataNascimento = table.Column<DateTime>(nullable: false),
+                    Escolaridade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
